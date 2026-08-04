1cf645cb62271cbe62c876bf21b1ce67e81a40aa
Owen <owenpalfreyman09@gmail.com>
2026-08-04 19:20:34 +0200

    Add typer and basic commands

--- main.py
+++ main.py
@@ -1,6 +1,19 @@
-def main():
-    print("Hello from atlas!")
+import typer
+
+app = typer.Typer()
+
+@app.command()
+def hello(name: str):
+    typer.echo(f"Hello, {name}")
+
+@app.command()
+def goodbye(name: str, formal: bool = False):
+    if formal:
+        typer.echo(f"Goodbye, Mr. {name}")
+    else:
+        typer.echo(f"Goodbye, {name}")
+
 
 
 if __name__ == "__main__":
-    main()
+    app()
