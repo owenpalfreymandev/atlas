4d41ac63c498c215b4a08e9c7ae972695e0d4aaf
Owen <owenpalfreyman09@gmail.com>
2026-08-04 19:47:22 +0200

    Move command files

--- main.py
+++ main.py
@@ -1,19 +1,4 @@
-import typer
-
-app = typer.Typer()
-
-@app.command()
-def hello(name: str):
-    typer.echo(f"Hello, {name}")
-
-@app.command()
-def goodbye(name: str, formal: bool = False):
-    if formal:
-        typer.echo(f"Goodbye, Mr. {name}")
-    else:
-        typer.echo(f"Goodbye, {name}")
-
-
+from app.commands import app
 
 if __name__ == "__main__":
     app()
