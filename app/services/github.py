f5f01ee7e850d67773cc278f95658b346655efce
Owen <owenpalfreyman09@gmail.com>
2026-08-05 21:45:47 +0200

    Create service functions

--- app/services/github.py
+++ app/services/github.py
@@ -21,6 +21,39 @@ def get_authenticated_user():
 
     return response.json()
 
+
+def get_user_repos():
+    token = get_token()
+
+    if not token:
+        raise RuntimeError("Not authenticated with GitHub. Run `auth login`.")
+
+    headers = {
+        "Authorization": f"token {token}",
+        "Accept": "application/vnd.github.v3+json"
+    }
+
+    response = requests.get(f"{GITHUB_API}/user/repos", headers=headers, timeout=10) 
+    response.raise_for_status()
+
+    return response.json()
+
+
+def get_repo_details(owner: str, repo: str):
+    token = get_token()
+
+    if not token:
+        raise RuntimeError("Not authenticated with GitHub. Run `auth login`.")
+
+    headers = {
+        "Authorization": f"token {token}",
+        "Accept": "application/vnd.github.v3+json"
+    }
+
+    response = requests.get(f"{GITHUB_API}/repos/{owner}/{repo}", headers=headers, timeout=10)
+    response.raise_for_status()
+
+    return response.json()
+
 if __name__ == "__main__":
-    user = get_authenticated_user()
-    print(user)
+    print(get_repo_details("owenpalfreymandev", "atlas"))
