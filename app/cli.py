8f80677fe000358305fcffd6a5f2036e22495ea8
Owen <owenpalfreyman09@gmail.com>
2026-08-05 22:27:24 +0200

    Add list command to CLI

--- app/cli.py
+++ app/cli.py
@@ -1,9 +1,10 @@
 import typer
 
-from app.commands import hello, me, auth
+from app.commands import auth, me, repo
 
 app = typer.Typer()
 
 app.command(name="login")(auth.login)
 app.command(name="logout")(auth.logout)
 app.command(name="me")(me.me)
+app.command(name="list")(repo.list)
