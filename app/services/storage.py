d1843e18b5ea653a7cb187dac62060d1a6bf9ad0
Owen <owenpalfreyman09@gmail.com>
2026-08-05 16:19:14 +0200

    Save and retrieve GitHub token in config file

---
+++ app/services/storage.py
@@ -0,0 +1,35 @@
+from pathlib import Path
+import json
+
+
+ATLAS_DIR = Path.home() / ".atlas"
+CONFIG_FILE = ATLAS_DIR / "config.json"
+
+
+def save_token(token: str):
+    ATLAS_DIR.mkdir(exist_ok=True)
+
+    data = {
+        "github_token": token
+    }
+
+    CONFIG_FILE.write_text(
+        json.dumps(data, indent=4),
+        encoding="utf-8"
+    )
+
+
+def get_token():
+    if not CONFIG_FILE.exists():
+        return None
+
+    data = json.loads(
+        CONFIG_FILE.read_text(encoding="utf-8")
+    )
+
+    return data.get("github_token")
+
+
+def clear_token():
+    if CONFIG_FILE.exists():
+        CONFIG_FILE.unlink()
