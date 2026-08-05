41621f38a94641f802988c53462d7416422e39ea
Owen <owenpalfreyman09@gmail.com>
2026-08-05 21:44:33 +0200

    Removed test code

--- app/cli.py
+++ app/cli.py
@@ -4,8 +4,6 @@ from app.commands import hello, me, auth
 
 app = typer.Typer()
 
-app.command(name="hello")(hello.hello)
-app.command(name="goodbye")(hello.goodbye)
 app.command(name="login")(auth.login)
 app.command(name="logout")(auth.logout)
 app.command(name="me")(me.me)
